5e4d07b149cbc2c071342c2943fc54cd55ba3d36
jukkamulla <jukkamulla@gmail.com>
2015-01-16 20:49:01 +0100

    add test version of game map

--- bbg.js
+++ bbg.js
@@ -1,1 +1,14 @@
 
+var bubbleContainer = document.getElementsByClassName("bubble-container")[0];
+var dimension = 5;
+
+//bubbleContainer.innerHTML = '<div class="bubble bubble-row"></div>';
+
+for (var i=0; i < dimension; i++) {
+    for (var j=0; j < dimension; j++) {
+        var newDiv = document.createElement('div');
+        newDiv.classList.add("bubble");
+        newDiv.classList.add("bubble-row");
+        bubbleContainer.appendChild(newDiv);
+    }
+}
